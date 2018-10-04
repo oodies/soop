374ca1968ab845cc28d656a539a62c34047d0cf7
schomy <sebastien.chomy@gmail.com>
2018-10-04 07:17:07 +0200

    build(composer): add the main dependencies of the project
    Add the orm-pack + configuration
    #composer require symfony/orm-pack
    
    Issue #2

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ * This file is part of SOOP project.
+ *
+ * (c) Sébastien CHOMY <sebastien.chomy@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class            => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class   => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class             => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
